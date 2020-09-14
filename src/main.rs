a9a729d313cda02d032a2e9331a65b70c03de590
Timmy Xiao <timmyxiao1@gmail.com>
2020-09-13 22:45:23 -0400

    Only match cringe as word

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,20 @@ use serenity::{
     model::{channel::Message, gateway::Ready},
     prelude::*,
 };
+use regex::Regex;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref CRINGE_REGEX: Regex = Regex::new(r"(?im)\bcringe\b").unwrap();
+}
+const CRINGE_RESPONSE: &str = "#42977 It's so funny to see manchildren use the word cringe everywhere. They're like 10 year olds calling anything they don't like \"gross\". But cringer.";
 
 struct Handler;
 
 impl EventHandler for Handler {
     fn message(&self, ctx: Context, msg: Message) {
-        if msg.content.contains("cringe") && !msg.author.bot {
-            let cringe_response = "#42977 It's so funny to see manchildren use the word cringe everywhere. They're like 10 year olds calling anything they don't like \"gross\". But cringer.";
-            if let Err(why) = msg.channel_id.say(&ctx.http, cringe_response) {
+        if CRINGE_REGEX.is_match(&msg.content) && !msg.author.bot {
+            if let Err(why) = msg.channel_id.say(&ctx.http, CRINGE_RESPONSE) {
                 println!("Error sending message: {:?}", why);
             }
         }
